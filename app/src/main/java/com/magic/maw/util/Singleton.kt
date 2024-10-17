4ca266adc0ab5ea309c661b949ed2c7c3f32125c
yangbin1023 <2452502323@qq.com>
2024-10-17 11:28:15 +0800

    Fix: Certificate errors when requesting data on some devices

--- app/src/main/java/com/magic/maw/util/Singleton.kt
+++ app/src/main/java/com/magic/maw/util/Singleton.kt
@@ -1,5 +1,6 @@
 package com.magic.maw.util
 
+import android.annotation.SuppressLint
 import android.os.Handler
 import android.os.HandlerThread
 import com.tencent.mmkv.MMKV
@@ -11,6 +12,10 @@ import io.ktor.client.plugins.cookies.HttpCookies
 import io.ktor.http.ContentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
 
 val kv by lazy { MMKV.mmkvWithID("config") }
 
@@ -20,6 +25,22 @@ val json by lazy {
         explicitNulls = true
         encodeDefaults = true
     }
+}
+
+@SuppressLint("TrustAllX509TrustManager", "CustomX509TrustManager")
+private val myTrustManager = object : X509TrustManager {
+    //checkServerTrusted和checkClientTrusted 这两个方法好像是用于，server和client双向验证
+    override fun checkServerTrusted(chain: Array<X509Certificate?>?, authType: String?) {}
+    override fun checkClientTrusted(chain: Array<X509Certificate?>?, authType: String?) {}
+    override fun getAcceptedIssuers(): Array<X509Certificate?> {
+        return arrayOfNulls(0)
+    }
+}
+
+private val mySSLSocketFactory by lazy {
+    val sslCtx = SSLContext.getInstance("TLS")
+    sslCtx.init(null, arrayOf(myTrustManager), SecureRandom())
+    sslCtx.socketFactory
 }
 
 val client by lazy {
@@ -33,6 +54,7 @@ val client by lazy {
         engine {
             config {
                 hostnameVerifier { _, _ -> true }
+                sslSocketFactory(mySSLSocketFactory, myTrustManager)
 //                proxy()
             }
         }
